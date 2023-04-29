909fa2581f209595bc8974f8974e5d9ee2bcef64
leminhbao308 <besilent308@gmail.com>
2023-04-29 21:51:41 +0700

    Fix iText7 to iText5, add Font to use, add PrintPDF controller, add new folder to store PDFTicket

--- Tour_Management/src/Test.java
+++ Tour_Management/src/Test.java
@@ -1,11 +1,35 @@
+import java.io.IOException;
+import java.util.Date;
+
+import com.itextpdf.text.DocumentException;
+
+import controllers.PDFCreator;
+import models.DiaDanh;
+import models.DichVu;
+import models.KhachHang;
+import models.TourDuLich;
+import models.VeTour;
+import utils.LoadSave;
 import utils.PasswordEncoder;
 
 public class Test {
 
-	public static void main(String args[]) {
-		String pass = "LeMinhBao21007881";
-		String code = PasswordEncoder.hashPassword(pass);
-		System.out.println(code);
-		System.out.println(PasswordEncoder.checkPassword(pass, code));
+    public static void main(String args[]) {
+	try {
+	    PDFCreator.createTicketPDF(
+		    new VeTour("VT001",
+			    new KhachHang("KH001", "051203000318", "Lê Minh Bảo", true, new Date(), "Gò Vấp",
+				    "leminhbao.iuh@gmail.com", "0399405711", 1900000f,
+				    new TourDuLich("TOUR001", "Quảng Ngãi", "Tour Quảng Ngãi", new Date(), "Quảng Ngãi",
+					    3, 1900000f, false, new DiaDanh("DD001", "Quảng Ngãi", "Quảng Ngãi")),
+				    new DichVu("DV001", "Xe Khách 40 chỗ", "Có khách sạn 3 sao", 1000000f))),
+		    LoadSave.GetSpriteAtlas(LoadSave.LOGO_LABEL));
+	} catch (DocumentException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
+    }
 }
