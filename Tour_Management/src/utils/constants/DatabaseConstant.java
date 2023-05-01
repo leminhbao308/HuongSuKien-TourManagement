7d9e45f2a5352045cb4080199706304994de2603
WolfLinh <linhspez@gmail.com>
2023-05-02 01:47:10 +0700

    Nhat Linh change path new DB

--- Tour_Management/src/utils/constants/DatabaseConstant.java
+++ Tour_Management/src/utils/constants/DatabaseConstant.java
@@ -7,7 +7,7 @@ package utils.constants;
  *
  */
 public class DatabaseConstant {
-    public static final String DB_URL = "jdbc:sqlserver://localhost:1433;databasename=DBQuanLyDuLich";
+    public static final String DB_URL = "jdbc:sqlserver://localhost:1433;databasename=QuanLyDuLich";
     public static final String USER = "sa";
     public static final String PASS = "sapassword";
 }
